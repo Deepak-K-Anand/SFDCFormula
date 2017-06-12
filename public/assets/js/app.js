79d8ac65ea78c391fc11d9587003ae58935a5c4c
Deepak K Anand <deepak@dazeworks.com>
2017-06-12 19:01:29 +0530

    Added Settings functionality

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -8,18 +8,87 @@ var App = (
         var __editor = null
             ,__aceLangTools;
 
+        ///////////////////////
+        //Settings Functions //
+        ///////////////////////
+        var __getSettings = function() {
+            var settings = {};
+
+            if( localStorage.settings ) {
+                settings = JSON.parse( localStorage.settings );
+            }
+
+            return settings;
+        };
+
+        var __setSettings = function( newSettings ) {
+            var existingSettings = __getSettings();
+
+            for( var key in newSettings ) {
+                existingSettings[key] = newSettings[key];
+            }
+
+            localStorage.settings = JSON.stringify( existingSettings );
+        };
+
+        var __createDefaultSettings = function() {
+            var defSettings = __getSettings();
+
+            if( Object.keys( defSettings ).length === 0 ) {
+                defSettings = {
+                    mode        : "ace/mode/sfformula",
+                    theme       : "ace/theme/monokai",
+                    fontSize    : "16"
+                };
+
+                __setSettings( defSettings );
+            }
+        };
+
+        var __loadEditorSettings = function() {
+            var settings = __getSettings();
+
+            //Setting language
+            __editor.session.setMode( settings.mode );
+
+            //Set theme
+            __editor.setTheme( settings.theme );
+
+            //Set font size
+            __editor.setFontSize( parseInt( settings.fontSize ) );
+
+            jQuery( "[data-settings]" ).each(
+                function() {
+                    var key = jQuery( this ).data( "settings" );
+
+                    jQuery( this ).val( settings[key] );
+                }
+            );
+        };
+
+        var __saveEditorSettings = function() {
+            var newSettings = {}; 
+
+            jQuery( "[data-settings]" ).each(
+                function() {
+                    newSettings[ jQuery( this ).data( "settings" ) ] = jQuery( this ).val();
+                }
+            );
+
+            __setSettings( newSettings );
+        };
+
         ////////////////////
         //Ace Initializer //
         ////////////////////
         var __initAce = function() {
+            __createDefaultSettings();
+
             //Lang Tools
             __aceLangTools = ace.require( "ace/ext/language_tools" );
 
             //Init editor
             __editor = ace.edit( "formula-editor" );
-
-            //Setting language
-            __editor.session.setMode( "ace/mode/sfformula" );
 
             //Configure various options
             __editor.setOptions(
@@ -30,11 +99,7 @@ var App = (
                 }
             );
 
-            //Set font size
-            __editor.setFontSize( 16 );
-
-            //Set theme
-            __editor.setTheme( "ace/theme/monokai" );
+            __loadEditorSettings();
         };
 
         //////////////////
@@ -69,5 +134,18 @@ var App = (
         };
 
         __init();
+
+        return {
+            loadEditorSettings : function() {
+                __loadEditorSettings();
+            },
+            saveEditorSettings : function() {
+                __saveEditorSettings();
+
+                __loadEditorSettings();
+
+                jQuery( "#mdlSettings" ).modal( "hide" );
+            }
+        }
     }
 )();
